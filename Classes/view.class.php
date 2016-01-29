05480b476be184c2f4378ff48cde4deef0d0033e
Nikelse <felnik@outlook.fr>
2016-01-29 21:29:10 +0100

    Create view.class.php

---
+++ Classes/view.class.php
@@ -0,0 +1,38 @@
+<?php 
+
+/* DBZ VIEW */
+
+class View {
+  
+    public function __construct () { }
+    
+    // menu list of table link
+    public static function MenuTable ($db_name, $array_table) {
+      $menu = "<div>DB : ".$db_name;
+      
+      foreach ($array_table as $K => $TABLE) {
+        $menu .= " <a href='?T=".$TABLE[0]."'>[ ".strtoupper($TABLE[0])." ]</a>";
+      }
+      
+      $menu .= "</div>";
+      
+      return $menu;
+    }    
+    
+    // html final rendering
+    public static function HTML ($title, $contener) {
+      echo "<html>
+      <head>
+        <title>".$title."</title>
+        <link rel='stylesheet' type='text/css' href='Fichiers/css/style.css' />
+      </head>
+      <body>
+        <img src='Fichiers/images/logo.jpg' /><br /><hr />
+        </hr>".$contener."
+      </body>
+      </html>";
+    }
+    
+}
+
+?>
