4b840a444e2482c35f8c37a19e36f8ba19f8c3e6
Nikelse <felnik@outlook.fr>
2016-01-29 21:24:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+/* DBZ FRONTAL CONTROLLER
+** MVC CMS for database management */
+
+// configuration
+require_once("Config/config.script.php");
+
+// connexion db
+require_once("Classes/pdo.connexion.class.php");
+$PDO = new Pdo_Connexion ($CONFIG['DB_INI_FILE']);
+
+// model class
+require_once("Classes/model.class.php");
+$MODEL = new Model ($PDO->CNX);
+
+// view class
+require_once("Classes/view.class.php");
+
+// html output increment
+$OUTPUT = NULL;
+
+// set the menu based on tables
+$OUTPUT .= View::MenuTable ($MODEL->Name_DB(), $MODEL->List_Table());
+
+
+
+
+// output echo screen rendering 
+View::HTML($CONFIG['MODULE_NAME'], $OUTPUT);
+
+?>
