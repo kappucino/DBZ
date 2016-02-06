4fb0ded4b7c73bb2b5ac22c38a8933124e664fdb
kappucino <nic.olivier@hotmail.fr>
2016-02-06 09:41:44 +0100

    qqsd

---
+++ defaut.php
@@ -0,0 +1,10 @@
+<?php
+
+$EXPORT .= "
+<li> <a href='?dir=1&T=".$_GET['T']."'>ajout</a></li>
+<li> <a href='?dir=2&T=".$_GET['T']."'>suppr</a></li>
+<li> <a href='?dir=3&T=".$_GET['T']."'>montrer</a></li>
+";
+
+
+?>
---
+++ montre.php
@@ -0,0 +1,7 @@
+<?php
+
+$MODEL->liste_val($_GET['T']);
+
+echo  View::val_tab($MODEL->Liste_val($_GET['T']));
+
+?>
